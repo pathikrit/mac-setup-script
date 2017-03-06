2a5cd5ecd142b6e0c9f70687f2515d87037034d2
Pathikrit Bhowmick <pathikritbhowmick@msn.com>
2017-03-06 17:30:29 -0500

    Use rvm to upgrade ruby

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ brews=(
   cheat
   clib
   coreutils
-  dfc
+  dfc             # disk viz
   findutils
   fontconfig --universal
   fpp
@@ -17,7 +17,6 @@ brews=(
   git-lfs
   gnuplot --with-qt
   go
-  gpg
   hh
   htop
   httpie
@@ -25,7 +24,6 @@ brews=(
   imagemagick
   lighttpd
   lnav
-  lumen
   mackup
   macvim
   mas
@@ -33,7 +31,7 @@ brews=(
   mtr
   ncdu
   nmap
-  node --with-full-icu
+  node
   poppler
   postgresql
   pgcli
@@ -73,7 +71,6 @@ casks=(
   istat-server  
   launchrocket
   licecap
-  lumen
   iterm2
   qlcolorcode
   qlmarkdown
@@ -208,11 +205,15 @@ function install {
 }
 
 echo "Installing ruby ..."
-brew install ruby-install chruby
-ruby-install ruby
-# TODO: enable auto switch here by following instructions
-echo "ruby-2.3.1" > ~/.ruby-version
 ruby -v
+brew install gpg
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+curl -sSL https://get.rvm.io | bash -s stable
+ruby_version='2.6.0'
+rvm install ${ruby_version}
+rvm use ${ruby_version} --default
+ruby -v
+sudo gem update --system
 
 echo "Installing Java ..."
 brew cask install java
@@ -256,18 +257,16 @@ done
 gpg --keyserver hkp://pgp.mit.edu --recv ${gpg_key}
 
 echo "Installing mac CLI ..."
+# Note: Say NO to bash-completions since we have fzf!
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/guarinogabriel/mac-cli/master/mac-cli/tools/install)"
 
 echo "Updating ..."
-pip install --upgrade setuptools
-pip install --upgrade pip
-gem update --system
+pip3 install --upgrade pip setuptools wheel
 mac update
 
 echo "Cleaning up ..."
 brew cleanup
 brew cask cleanup
-brew linkapps
 
 for fail in ${fails[@]}
 do
@@ -282,8 +281,6 @@ echo "Run `mackup restore` after DropBox has done syncing"
 #mkdir -p ~/.config/fish/
 #echo "source /usr/local/share/chruby/chruby.fish" >> ~/.config/fish/config.fish
 #echo "source /usr/local/share/chruby/auto.fish" >> ~/.config/fish/config.fish
-#echo "export GOPATH=/usr/libs/go" >> ~/.config/fish/config.fish
-#echo "export PATH=$PATH:$GOPATH/bin" >> ~/.config/fish/config.fish
 #chsh -s $(which fish)
 #curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install | fish
 #for omf in ${omfs[@]}
