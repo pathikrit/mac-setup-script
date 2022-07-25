e5e796c19be12bdcbeed2a060367520535e429df
Will Chiong <infomofo@users.noreply.github.com>
2022-07-24 20:59:42 -0400

    Personalize (#1)
    * Personalize
    
    * update cask install syntax
    
    * tmp remove awscli
    
    * rm go
    
    * try macvim instead of vim
    
    * update font install instructions
    
    * remove unused sections
    
    * shellcheck lint

--- defaults.sh
+++ defaults.sh
@@ -10,13 +10,8 @@ if [[ -z "${CI}" ]]; then
   while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 fi
 
-# Trackpad: enable tap to click for this user and for the login screen
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-
 # Disable the sound effects on boot
-sudo nvram SystemAudioVolume=" "
+# sudo nvram SystemAudioVolume=" "
 
 # Close any open System Preferences panes, to prevent them from overriding settings we’re about to change
 osascript -e 'tell application "System Preferences" to quit'
@@ -48,11 +43,12 @@ defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false  
 defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false   # Disable smart quotes
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false  # Disable auto-correct
 
+# Disable Natural Scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+defaults write NSGlobalDomain AppleEnableSwipeNavigateWithScrolls -bool false
+
 # Enable full keyboard access for all controls e.g. enable Tab in modal dialogs
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
-# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
-defaults write com.apple.finder QuitMenuItem -bool true
 
 # Set Desktop as the default location for new Finder windows
 defaults write com.apple.finder NewWindowTarget -string "PfDe"
@@ -72,9 +68,13 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
 # Automatically open a new Finder window when a volume is mounted
-defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
-defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
-defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+# defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+# defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+# defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+
+# Set hot corners
+defaults write com.apple.dock wvous-bl-corner -int 5
+defaults write com.apple.dock wvous-br-corner -int 10
 
 # Use list view in all Finder windows by default (codes for the other view modes: `icnv`, `clmv`, `Flwv`)
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
@@ -107,3 +107,10 @@ defaults write com.apple.ActivityMonitor ShowCategory -int 0
 # Sort Activity Monitor results by CPU usage
 defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
 defaults write com.apple.ActivityMonitor SortDirection -int 0
+
+# Disable system screenshot sound
+defaults write com.apple.systemsound "com.apple.sound.uiaudio.enabled" -int 0
+
+# Disable Notification Center and remove the menu bar icon
+launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
+
--- install.sh
+++ install.sh
@@ -3,114 +3,48 @@
 # Install some stuff before others!
 important_casks=(
   authy
-  dropbox
+  firefox
   google-chrome
-  hyper
+  iterm2
   jetbrains-toolbox
-  stats
-  spotify
-  visual-studio-code
   slack
+  visual-studio-code
 )
 
 brews=(
   ##### Install these first ######
-  awscli
-  gimme-aws-creds  
+  # awscli
+  bash
+  circleci
   git
-  jabba
+  github/gh/gh
   python3
   sbt  
   scala
-  xonsh
   ################################
-  "bash-snippets --without-all-tools --with-cryptocurrency --with-stocks --with-weather"
-  bat
-  #cheat         # https://github.com/cheat/cheat
   coreutils
-  dfc           # https://github.com/rolinh/dfc
-  exa           # https://the.exa.website/
-  findutils
-  "fontconfig --universal"
-  git-extras    # for git undo
-  git-lfs
-  "gnuplot --with-qt"
-  "gnu-sed --with-default-names"
-  go
-  gpg
   #hosts
-  hstr          # https://github.com/dvorka/hstr
-  htop          # https://htop.dev/
-  httpie        # https://httpie.io/
-  iftop         # https://www.ex-parrot.com/~pdw/iftop/
   "imagemagick --with-webp"
-  lnav          # https://lnav.org/
-  m-cli         # https://github.com/rgcr/m-cli
   macvim        # https://macvim-dev.github.io/macvim/
-  micro         # https://github.com/zyedidia/micro
-  mtr           # https://www.bitwizard.nl/mtr/
-  neofetch      # https://github.com/dylanaraps/neofetch
   node
-  poppler       # https://poppler.freedesktop.org/
-  postgresql
-  pgcli
-  pv            # https://www.ivarch.com/programs/pv.shtml
   python 
-  osquery
-  ruby
-  shellcheck    # https://www.shellcheck.net/
-  thefuck       # https://github.com/nvbn/thefuck
+  reattach-to-user-namespace # this enables copy/paste for vi within tmux
   tmux
   tree
-  trash
-  "vim --with-override-system-vi"
+  # "vim --with-override-system-vi"
   "wget --with-iri"
-  xsv
-  youtube-dl
 )
 
 casks=(
-  aerial
-  adobe-acrobat-pro
   cakebrew
+  calibre
+  discord
   cleanmymac
-  docker
-  expressvpn
-  firefox
-  geekbench
-  google-backup-and-sync
-  github
-  handbrake
-  iina
-  istat-server
-  kap
-  keepingyouawake
-  launchrocket
-  little-snitch
-  macdown
-  monitorcontrol
-  muzzle
-  private-eye
-  qlcolorcode
-  qlmarkdown
-  qlstephen
-  quicklook-json
-  quicklook-csv
-  satellite-eyes
-  sidekick
-  sloth
-  soundsource
   steam
-  synergy
-  transmission
-  xquartz
 )
 
 pips=(
   pip
-  glances
-  ohmu
-  pythonpy
 )
 
 gems=(
@@ -122,8 +56,7 @@ npms=(
   n           # https://github.com/tj/n
 )
 
-gpg_key='3E219504'
-git_email='pathikritbhowmick@msn.com'
+git_email='will.chiong@gmail.com'
 git_configs=(
   "branch.autoSetupRebase always"
   "color.ui auto"
@@ -136,29 +69,20 @@ git_configs=(
   "rerere.autoUpdate true"
   "remote.origin.prune true"
   "rerere.enabled true"
-  "user.name pathikrit"
+  "user.name Will Chiong"
   "user.email ${git_email}"
-  "user.signingkey ${gpg_key}"
 )
 
 vscode=(
-  alanz.vscode-hie-server
-  justusadam.language-haskell
-  ms-vsonline.vsonline
-  ms-ossdata.vscode-postgresql
-  rebornix.ruby
-  redhat.java
-  rust-lang.rust
   scalameta.metals
   scala-lang.scala
 )
 
 fonts=(
   font-fira-code
+  font-jetbrains-mono
   font-source-code-pro
 )
-
-JDK_VERSION=amazon-corretto@1.8.222-10.1
 
 ######################################## End of app list ########################################
 set +e
@@ -166,7 +90,7 @@ set -x
 
 function prompt {
   if [[ -z "${CI}" ]]; then
-    read -p "Hit Enter to $1 ..."
+    read -p -r "Hit Enter to $1 ..."
   fi
 }
 
@@ -181,7 +105,7 @@ function install {
       echo "Installed $pkg"
     else
       echo "Failed to execute: $exec"
-      if [[ ! -z "${CI}" ]]; then
+      if [[ -n "${CI}" ]]; then
         exit 1
       fi
     fi
@@ -222,26 +146,20 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 
 echo "Install important software ..."
 brew tap homebrew/cask-versions
-install 'brew cask install' "${important_casks[@]}"
+install 'brew install --cask' "${important_casks[@]}"
 
 prompt "Install packages"
 install 'brew_install_or_upgrade' "${brews[@]}"
 brew link --overwrite ruby
 
-prompt "Install JDK=${JDK_VERSION}"
-curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
-jabba install ${JDK_VERSION}
-jabba alias default ${JDK_VERSION}
-java -version
-
 prompt "Set git defaults"
 for config in "${git_configs[@]}"
 do
-  git config --global ${config}
+  git config --global "${config}"
 done
 
 if [[ -z "${CI}" ]]; then
-  gpg --keyserver hkp://pgp.mit.edu --recv ${gpg_key}
+  # gpg --keyserver hkp://pgp.mit.edu --recv ${gpg_key}
   prompt "Export key to Github"
   ssh-keygen -t rsa -b 4096 -C ${git_email}
   pbcopy < ~/.ssh/id_rsa.pub
@@ -249,35 +167,19 @@ if [[ -z "${CI}" ]]; then
 fi  
 
 prompt "Upgrade bash"
-brew install bash bash-completion2 fzf
 sudo bash -c "echo $(brew --prefix)/bin/bash >> /private/etc/shells"
 sudo chsh -s "$(brew --prefix)"/bin/bash
-# Install https://github.com/twolfson/sexy-bash-prompt
-touch ~/.bash_profile # see https://github.com/twolfson/sexy-bash-prompt/issues/51
-(cd /tmp && git clone --depth 1 --config core.autocrlf=false https://github.com/twolfson/sexy-bash-prompt && cd sexy-bash-prompt && make install) && source ~/.bashrc
-hstr --show-configuration >> ~/.bashrc
-
-echo "
-alias del='mv -t ~/.Trash/'
-alias ls='exa -l'
-alias cat=bat
-" >> ~/.bash_profile
-
-prompt "Setting up xonsh"
-sudo bash -c "which xonsh >> /private/etc/shells"
-sudo chsh -s $(which xonsh)
-echo "source-bash --overwrite-aliases ~/.bash_profile" >> ~/.xonshrc
 
 prompt "Install software"
-install 'brew cask install' "${casks[@]}"
+install 'brew install --cask' "${casks[@]}"
 
 prompt "Install secondary packages"
 install 'pip3 install --upgrade' "${pips[@]}"
 install 'gem install' "${gems[@]}"
 install 'npm install --global' "${npms[@]}"
 install 'code --install-extension' "${vscode[@]}"
-#brew tap caskroom/fonts
-#install 'brew cask install' "${fonts[@]}"
+brew tap homebrew/cask-fonts
+install 'brew install --cask' "${fonts[@]}"
 
 prompt "Update packages"
 pip3 install --upgrade pip setuptools wheel
